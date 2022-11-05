16f195f4054a397d267aa059e50beadf9d93ed25
Miriam <sh.miriam8@gmail.com>
2022-11-05 23:22:30 +0200

    Add project files.

---
+++ dotNet5783_8405_0000/Program0000.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5783_8405_0000
+{
+    internal partial  class Program
+
+    {
+      
+        static partial void Hello0000()
+        {
+            Console.WriteLine("Hello, whats your name?");
+
+            string? n = Console.ReadLine();
+
+            Console.WriteLine($"Welcome {n}");
+        }
+    }
+}
---
+++ dotNet5783_8405_0000/Program8405.cs
@@ -0,0 +1,19 @@
+﻿partial class Program8405
+{
+    public  static  void Main()
+    {
+        Hellow8405();
+        Hellow0000();
+        Console.ReadLine();
+    }
+    static partial void Hellow0000();
+
+ static void Hellow8405()
+    {
+        Console.WriteLine("Hello, whats your name?");
+
+        string? n = Console.ReadLine();
+
+        Console.WriteLine($"Welcome {n}");
+    }
+}
